64c56ab0e82e4923e0e88263f815467b560f29fa
Phillip Beltre <phillipbeltre@Phillips-MacBook-Pro.local>
2019-06-09 13:17:20 -0400

    setup basic node server

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 console.log("hi")
-const melodies = require('./preset-melodies.js')
-console.log(JSON.stringify(melodies))
+const express = require('express')
+const melodies = require('./preset-melodies.json')
+// console.log(JSON.stringify(melodies))
+
+// const fs = require('fs');
+// let rawdata = fs.readFileSync('student.json');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+const port = process.env.PORT || 8082
+
+app.listen(port, () => console.log(`server running on port ${port}`))
