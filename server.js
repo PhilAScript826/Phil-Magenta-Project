dab51f3eb4b8b04c3dd10ffd17b0f302e6aa06db
Phillip Beltre <phillipbeltre@Phillips-MacBook-Pro.local>
2019-06-09 14:00:26 -0400

    commit before breaking stuff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 console.log("hi")
 const express = require('express')
 const melodies = require('./preset-melodies.json')
+const fs = require('fs-extra');
 // console.log(JSON.stringify(melodies))
 
 const app = express();
@@ -13,5 +14,8 @@ const port = process.env.PORT || 8082
 
 app.listen(port, () => console.log(`server running on port ${port}`))
 
+var checkpoint = fs.readdirSync('/private/tmp/music_vae/train');
+
+console.log(checkpoint)
 //nodemon -> npm package that listens for your changes and restarts the server each time you save file.
 //express => node framework (similar to django and python)
