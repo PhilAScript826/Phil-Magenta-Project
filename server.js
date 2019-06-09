cb82f60ae77d3b6da622e83f5a4cdfd2482d3ad7
Phillip Beltre <phillipbeltre@Phillips-MacBook-Pro.local>
2019-06-09 13:37:55 -0400

    render html

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const express = require('express')
 const melodies = require('./preset-melodies.json')
 // console.log(JSON.stringify(melodies))
 
-// const fs = require('fs');
-// let rawdata = fs.readFileSync('student.json');
-
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.sendFile(__dirname+'/index.html') //Renders index.html when you do get request from route
 })
 
 const port = process.env.PORT || 8082
 
 app.listen(port, () => console.log(`server running on port ${port}`))
+
+//nodemon -> npm package that listens for your changes and restarts the server each time you save file.
+//express => node framework (similar to django and python)
